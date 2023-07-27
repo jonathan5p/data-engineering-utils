064cdabcb3cce111a42341aea066919d1d5534fa
Jonathan Roncancio <jonathanpinzon05@gmail.com>
2023-07-27 11:29:16 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from de_utils import __version__
+
+setup(
+    name="de_utils",
+    version=__version__,
+    url="https://github.com/jonathan5p/data-engineering-utils",
+    author="Jonathan Roncancio",
+    author_email="jonathanpinzon05@gmail.com",
+    py_modules=["de_utils"],
+)
